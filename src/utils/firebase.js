0e74fbf053229e87f7aedc7ce0d299b42a1342be
Melwyn <ms24.melwyn.peter@kccemsr.edu.in>
2026-05-20 15:40:32 +0530

    Secured Firebase config with env variables

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,24 +1,18 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getFirestore, serverTimestamp } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Vite uses import.meta.env to read variables from the .env file
 const firebaseConfig = {
-  apiKey: "AIzaSyAWh4oVfSZYNOngiJx4rBQ2jrIkeXrd4OE",
-  authDomain: "gform-ebe75.firebaseapp.com",
-  projectId: "gform-ebe75",
-  storageBucket: "gform-ebe75.firebasestorage.app",
-  messagingSenderId: "372727146544",
-  appId: "1:372727146544:web:215824c15f5e284a9f25f5",
-  measurementId: "G-GVCYFR6CV3"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-export const db = getFirestore(app);
-export { serverTimestamp };
+const db = getFirestore(app);
+
+export { db, serverTimestamp };
